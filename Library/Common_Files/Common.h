402238ceac81b8ec7b147718c4a96b26c8ad1714
Maxim Thibodeau <thibodeau.maxim@courrier.uqam.ca>
2025-06-08 18:32:19 -0400

    test8

--- Library/Common_Files/Common.h
+++ Library/Common_Files/Common.h
@@ -36,11 +36,13 @@
 #include <Windows.h>
 #elif defined __linux
 #endif
+
 #ifndef NOT_GIT
 #include <dlfcn.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #endif
+
 #ifdef __linux
 #define VK_KHR_XLIB_SURFACE_EXTENSION_NAME "VK_KHR_xlib_surface"
 #include <unistd.h>
